c553343d135973d2ff4980011a8f9565a2c88f4b
Clement <clementacole75@gmail.com>
2015-01-10 18:58:23 -0600

    my application

---
+++ chat.php
@@ -0,0 +1,190 @@
+<?php
+	/*Connect to MySQL*/
+	mysql_connect('host', 'database', 'password')
+	or die(mysql_error());
+	/*Select database*/
+	mysql_select_db('database')
+	or die(mysql_error());
+	/*Create the table*/
+	mysql_query("create table chat(time int(11) NOT NULL, name varchar(30) NOT NULL, ip varchar(15) NOT NULL, message varchar(500) NOT NULL, PRIMARY KEY (time)")or
+	die (mysql_error());
+	echo "Complete.";
+?>
+<html>
+		<head>
+			<style>
+				.message{
+					overflow:hidden;
+					width:498;
+					margin-bottom:5px;
+					border:1px solid #999;
+				}
+				.messagehead{
+					overflow:hidden;
+					background:#FFC;
+					width:500px;
+				}
+				.messagecontent{
+					overflow:hidden;
+					width:496px;
+				}
+			</style>
+		</head>
+
+		<body>
+			<div id="chat" style="width:500px;margin:0 auto;overflow:hidden;">
+				<!-- THIS div will contain the message -->
+	        		<div id="messages"></div>	
+				<!-- This div will contain an eventual error message --> 
+				<div id="error" style="width:500px;text-align:center;color:red;"></div> 
+				<!--This div contains the forms and the send button --> 
+			<div id="write" style="text-align:center;"><textarea id="message" cols="50" rows="5"></textarea><br/>Name:<input type="text" id="name"/><input type="button" value="Send" onClick="send();"/></div>
+		 </div>
+	</body>
+
+	<script type="text/javascript">
+	//This function will display the messages
+	function showmessages(){
+		//Send an XMLHttpRequest to the 'show-message.php' file
+		if(window.XMLHttpRequest){
+			xmlhttp = new XMLHttpRequest();
+			xmlhttp.open("GET", "show-messages.php", false);
+			xmlhttp.send(null);
+			}
+		else{
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			xmlhttp.open("GET", "showmessages.php",false);
+			xmlhttp.send();
+			}
+			//Replace the content of the messages with the response from the 'show-messages.php' file
+		document.getElementById('messages').innerHTML = xmlhttp.responseText;
+			//Repeat the function each 30 seconds
+		setTimeout('showmessages()',30000);
+			}
+			//Start the showmessages() function
+		showmessages();
+			//This function will submit the message
+		function send(){
+			//Send an XMLHttpRequest to the 'send.php' file with all the required informations
+		  var sendto = 'send.php?message=' +
+		document.getElementById('message').value + '&name=' +
+		document.getElementById('name').value;
+			if(window.XMLHttpRequest(){
+				xmlhttp = new XMLHttpRequest();
+				xmlhttp.open("GET", sendto,false);
+				xmlhttp.send(null);
+		   }
+			else{
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				xmlhttp.open("GET",sendto,false);
+				xmlhttp.send();
+		   }
+		   var error=' ';
+		 	//If an error occurs the 'send.php' file sends the number of the error
+			//and based on that number a message is displayed
+			switch(parseInt(xmlhttp.responseText)){
+				case 1:
+					error = 'The database is down!';
+					break;
+				case 2:
+					error = 'The database is down!';
+					break;
+				case 3:
+					error = 'Donnot forget the message!';
+					break;
+				case 4:
+					error = 'The message is too long!';
+					break;
+
+				case 5:
+					error = 'Donot forget the name!';
+					break;
+				case 6:
+					error = 'The name is too long!';
+					break;
+				case 7:
+					error = 'This name is already used by somebody else!;
+					break;
+				case 8:
+					error = 'The database is down!';
+			}
+			if(error = ''){
+				document.getElementById('error').innerHTML = '';
+				showmessages();
+			}
+			else{
+
+				document.getElementById('error').innerHTML = error;
+			}
+		}
+	</script>
+   </body>
+</html>
+
+
+
+
+	<?php
+		//Connect to MySQL
+		mysql_connect('host', 'database', 'password') or die(1);
+		//Select database
+		mysql_select_db('borcsa9_database') or die(2);
+		//Check if message is too long
+		else if(strlen($message) > 255){
+			echo 4;
+		}
+		//Check if name is empty
+		else if(strlen($name) < 1){
+			echo 5;
+		}
+		//Check if name is too long
+		else if(strlen($name)<1){
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+@import "css/menu.css";
+@import "js/menu.js";
+<head>
+	<title>GAMER BEEF</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="icon" type="image/png" href="/media/layout/favicon.png">
+
+	<link href="css/screen.css" rel="stylesheet" type="text/css" /> 
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery.tubular.1.0.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/index.js"></script>
+    
+</head>
+<body>
+	<div id="wrapper" class="clearfix">
+		<div id="main">
+			<a href="http://www.seanmccambridge.com/tabular/"><img src="media/gamers.png" alt="Tubular logo" id="logo" /></a>
+			<p id="video-controls" class="black-65">Video controls:<br /><a href="#" class="tubular-play">Play</a> | <a href="#" class="tubular-pause">Pause</a> | <a href="#" class="tubular-volume-up">Volume Up</a> | <a href="#" class="tubular-volume-down">Volume Down</a> | <a href="#" class="tubular-mute">Mute</a></p>
+			<h1>         ALRIGHT LADIES/GENTLEMEN HERE ARE THE RULES                   </h1>
+			<p>1. WE DONNOT CARE ABOUT WHAT YOU HAD FOR BREAKFAST,LUNCH, OR DINNER!!</p>
+			<p>2. WE DONNOT CARE ABOUT YOUR FEELINGS!</p>
+			<p>3. WE DONNOT CARE ABOUT USING PROFANITY!</p>
+			<p>4. WE DONNOT CARE ABOUT MISUSE OF LANGUAGE!</p>
+			<p>5. ALL WE CARE ABOUT IS PERFORMANCE ON YOUR CHOICE OF CONSOLE!</p>
+			<p>6. IF YOU CAN'T LIVE BY THESE FIVE RULES WELL THEN GUESS WHAT?</p>
+			<p>-----------------GET THE FUCK OUTER HERE!! -------------------</p>
+	
+		<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+		<link href="css/menu.css" rel="stylesheet"/>
+		<link href="js/menu.css" rel="javascript.js"/>
+			<h3>MENU</h3>
+			<a href="#">Solo</a>
+			<a href="#">Multiplayer</a>
+    			<a href="#">Teams/Clans</a>
+	        </nav>
+</body>
+</html>
